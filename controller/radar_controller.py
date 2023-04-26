47da1c1cfb625b3b3d14b68f6c270f17a1b41ae5
roganOh <fdscbjdcnhd@gmail.com>
2023-04-26 17:26:06 +0900

    make graph with data of python

--- controller/radar_controller.py
+++ controller/radar_controller.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, jsonify
 
 bp = Blueprint('radar', __name__)
 
@@ -6,3 +6,12 @@ bp = Blueprint('radar', __name__)
 @bp.route('/radar', methods=['GET'])
 def show_radar():
     return render_template('radar.html')
+
+
+@bp.route('/radar/data')
+def get_data():
+    data = {
+        'labels': ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
+        'values': [65, 59, 90, 81, 56, 55, 40]
+    }
+    return jsonify(data)
