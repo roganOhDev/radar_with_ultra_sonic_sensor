6f3da8f377d2ee26ea96d7bd48109d0e0a852615
roganOh <fdscbjdcnhd@gmail.com>
2023-04-27 10:07:45 +0900

    run version

--- const/config.py
+++ const/config.py
@@ -1,3 +1,14 @@
+from enum import Enum
+
 labels = ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running']
 max_distance = 100
 db_name = "test.db"
+
+
+class RunVersion(Enum):
+    debug = "DEBUG"
+    demo = "DEMO"
+
+
+version = RunVersion.debug
+# version = RunVersion.demo
--- const/database_config.py
+++ const/database_config.py
@@ -1,9 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from const.config import db_name
+from const.config import db_name, version, RunVersion
 
-engine = create_engine("sqlite+pysqlite:///./" + db_name, echo=True, future=True)
+engine = None
+if version == RunVersion.debug:
+    engine = create_engine("sqlite+pysqlite:///./" + db_name, echo=True, future=True)
+elif version == RunVersion.demo:
+    engine = create_engine("sqlite+pysqlite:///./" + db_name, echo=False, future=True)
+
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 
