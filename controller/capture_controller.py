13472679a137f4bf507c88c6af3665888bf58a86
roganOh <fdscbjdcnhd@gmail.com>
2023-04-27 14:17:47 +0900

    link in list

--- controller/capture_controller.py
+++ controller/capture_controller.py
@@ -19,7 +19,7 @@ def get_list_data() -> Response:
 
 @bp.route('/<id>')
 def show_data(id: int) -> str:
-    return render_template('capture.html', id=id)
+    return render_template('capture.html')
 
 
 @bp.route('/<id>/get')
