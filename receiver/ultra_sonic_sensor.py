95e72331d536ba869ea78166fd1a7e3c5c9700de
onjellyday <rlgns991019@gmail.com>
2023-05-06 15:50:55 +0900

    updated by hoon

--- receiver/ultra_sonic_sensor.py
+++ receiver/ultra_sonic_sensor.py
@@ -1,12 +1,11 @@
 from const.config import max_distance
 from utils.log import logger
-import random
+import serial
 
+coms = serial.Serial("COM3",baudrate=9600,timeout=0.001)
 
 def get_distance():
-    distance = random.randrange(max_distance)
-
-    logger.info(f"Distance From Sensor: {distance}")
-
-    return distance
-
+    distance=coms.readline().decode().strip()
+    print(distance)
+    logger.info(f"Distance From Sensor: {distance}")    
+    return  distance
